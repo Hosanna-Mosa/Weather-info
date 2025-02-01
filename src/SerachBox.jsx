32a4f9a0f0291c4feb00948c007e72a6b0736d9c
Hosanna Mosa <hosannamosa4190@gmail.com>
2025-02-01 07:19:02 +0530

    building

--- src/SerachBox.jsx
+++ src/SerachBox.jsx
@@ -6,7 +6,39 @@ import Typography from "@mui/material/Typography";
 import { useState } from "react";
 
 const SerachBox = () => {
+  const [value, setValue] = useState("");
  
+
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "b010b23c8a55d6b005c9dcd8f546efec";
+  
+  const getWeatherInfo = async () =>{
+    const response =  await fetch(`${API_URL}?q=${value}&appid=${API_KEY}`);
+    const jsonResponse  = await response.json();
+    // console.log(jsonResponse.main);
+    let resultfinal = {
+      temp : jsonResponse.main.temp,
+      feels_like : jsonResponse.main.feels_like,
+      humidity : jsonResponse.main.humidity,
+      pressure : jsonResponse.main.pressure,
+      temp_max : jsonResponse.main.temp_max,
+      temp_min : jsonResponse.main.temp_min
+    };
+    
+    
+  }
+
+  
+
+  const handleInput = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSumbit = (eve) => {
+    eve.preventDefault();
+    setValue("");
+    getWeatherInfo();
+  };
 
   return (
     <div className="SerachBox">
